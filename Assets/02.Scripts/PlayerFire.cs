9a98ead38d018ffe944b317e7fe7519b6777d148
qlsk <jhanggu0625@gmail.com>
2026-09-04 13:35:12 +0900

    feat: 총알 발사 쿨타임 추가

--- Assets/02.Scripts/PlayerFire.cs
+++ Assets/02.Scripts/PlayerFire.cs
@@ -9,24 +9,36 @@ public class PlayerFire : MonoBehaviour
     public GameObject LeftFirePosition;
     public GameObject RightFirePosition;
 
+    // 총알 쿨타입
+    public float CoolTime = 0.6f;
+    public float CoolTimer = 0;
+
     private void Start()
     {
+        CoolTimer = CoolTime;
     }
 
     private void Update()
     {
-        // 목표: 총알을 만들어서 발사하고 싶다.
+        CoolTimer -= Time.deltaTime;
 
-        // 1. 발사 버튼을 누르면
-        if (Input.GetButtonDown("Fire1"))
+        if (CoolTimer <= 0 && Input.GetButtonDown("Fire1"))
         {
-            // 2. 프리팹으로부터 총알 만들기
-            GameObject rightBullet = Instantiate(BulletPrefab);
-            GameObject leftBullet = Instantiate(BulletPrefab);
-
-            // 3. 총알 위치를 총구 위치로 바꾸기
-            rightBullet.transform.position = RightFirePosition.transform.position;
-            leftBullet.transform.position = LeftFirePosition.transform.position;
+            Fire();
+            CoolTimer = CoolTime;
         }
+    }
+
+    // 목표: 총알을 만들어서 발사하고 싶다.
+    public void Fire()
+    {
+        // 1. 발사 버튼을 누르면
+        // 2. 프리팹으로부터 총알 만들기
+        GameObject rightBullet = Instantiate(BulletPrefab);
+        GameObject leftBullet = Instantiate(BulletPrefab);
+
+        // 3. 총알 위치를 총구 위치로 바꾸기
+        rightBullet.transform.position = RightFirePosition.transform.position;
+        leftBullet.transform.position = LeftFirePosition.transform.position;
     }
 }
