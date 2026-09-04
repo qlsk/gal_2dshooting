964ff07c465fe015b1d099c6fcd625eac51da41f
qlsk <jhanggu0625@gmail.com>
2026-09-04 12:08:14 +0900

    feat: 플레이어 이동 기능 추가

---
+++ Assets/02.Scripts/PlayerMove.cs
@@ -0,0 +1,50 @@
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+    private void Start()
+    {
+    }
+
+    public float Speed = 5f;
+    public float MaxPositionY;
+    public float MinPositionY;
+    public float MaxPositionX;
+    public float MinPositionX;
+
+    private void Update()
+    {
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+        Vector2 dir = new Vector2(h, v);
+        dir = dir.normalized;
+        transform.position += Speed * Time.deltaTime * (Vector3)dir;
+
+        if (transform.position.y < MinPositionY)
+        {
+            transform.position = new Vector2(transform.position.x, MinPositionY);
+        }
+        else if (transform.position.y > MaxPositionY)
+        {
+            transform.position = new Vector2(transform.position.x, MaxPositionY);
+        }
+
+        if (transform.position.x < MinPositionX)
+        {
+            transform.position = new Vector2(MaxPositionX, transform.position.y);
+        }
+        else if (transform.position.x > MaxPositionX)
+        {
+            transform.position = new Vector2(MinPositionX, transform.position.y);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            Speed--;
+        }
+        else if (Input.GetKeyDown(KeyCode.E))
+        {
+            Speed++;
+        }
+    }
+}
